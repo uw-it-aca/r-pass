f1ea53fe1f37050b23217d0e54ee49e49508e892
Patrick Michaud <pmichaud@uw.edu>
2012-11-14 11:08:50 -0800

    Adds service, host and access token models.  adds django-fields to get encrypted fields

--- r_pass/models.py
+++ r_pass/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django_fields.fields import EncryptedTextField, EncryptedCharField
 
-# Create your models here.
+class Host(models.Model):
+    cname = models.CharField(max_length=250, unique=True)
+
+class Service(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    hosts = models.ManyToManyField(Host)
+
+class AccessToken(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    user = EncryptedCharField(max_length=150)
+    access_token = EncryptedTextField()
+    service = models.ForeignKey(Service)
+
