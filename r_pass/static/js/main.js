2f4caef0890b37b64dbea75b766654e037c2d7d3
Patrick Michaud <pmichaud@uw.edu>
2012-12-07 09:54:00 -0800

    Moving the random password block into a proper js file, using a local jquery file

--- r_pass/static/js/main.js
+++ r_pass/static/js/main.js
@@ -21,3 +21,23 @@
     }
 
 })(this);
+
+
+function make_random_password() {
+    var chars = []
+    for (i=48; i < 127; i++) {
+        chars.push(String.fromCharCode(i));
+    }
+
+    var passchars = []
+    for (i = 0; i < 30; i++) {
+        var rand = chars[Math.floor(Math.random() * chars.length)];
+        passchars.push(rand);
+    }
+
+    var password = passchars.join("");
+
+    document.getElementById("id_access_token").value = password;
+    return false;
+}
+
