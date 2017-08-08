9d52d3c07bccc9e56120ca5caa34e7158de29b98
Jim Laney <jlaney@uw.edu>
2017-08-08 09:58:04 -0700

    adds model tests

--- r_pass/tests.py
+++ r_pass/tests.py
@@ -3,8 +3,24 @@ from r_pass.models import AccessToken, Service
 
 
 class AccessTokenModelTest(TestCase):
+    def tearDown(self):
+        AccessToken.objects.all().delete()
+        Service.objects.all().delete()
+
     def test_save(self):
-        pass
+        service = Service(title="Test Service")
+        service.save()
+
+        token = AccessToken()
+        token.name = "Test Token"
+        token.user = "javerage"
+        token.access_token = "00000000"
+        token.service = service
+        token.save()
+
+        token = AccessToken.objects.get(name="Test Token")
+        self.assertEqual(token.user, "javerage")
+        self.assertEqual(token.access_token, "00000000")
 
 
 class ServiceModelTest(TestCase):
